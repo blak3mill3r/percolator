f84f0e7ef166ee9a5552f7777efadd09b36de31a
Blake Miller <blak3mill3r@gmail.com>
2012-08-19 09:46:49 -0700

    refactor....

--- src/percolator/interpreter.clj
+++ src/percolator/interpreter.clj
@@ -14,24 +14,27 @@
   (merge interpreters
          {scope {}}))
 
-(defn interpreter-for-scope-and-form [scope form]
-  (let [ root-interpreters (scope interpreters) ]
-    (do
-      ;(println (first form))
-      (root-interpreters (first form)))))
+;(defmethod interpreter-for-scope-and-form [scope form]
+;  (let [ root-interpreters (scope interpreters) ]
+;    (do
+;      ;(println (first form))
+;      (root-interpreters (first form)))))
 
-; dispatch based on the class of the 2nd arg, the form
-(defmulti interpret-in-scope #( class %2 ))
+(defn interpreter-for-scope-and-form [scope form]
+  (let [ root-interpreters (scope interpreters)
+         interpreter-key   (if (seq? form) (first form) (class form))
+        ]
+    (root-interpreters interpreter-key)))
 
 ;(isa? clojure.lang.ASeq (class '(quote this)))
 
 ; multimethod so literals can potentially have special scope-local meaning
 ; this is just for forms
 ; but for it to work it'll have to handle some (clj) literals
-(defmethod interpret-in-scope clojure.lang.ASeq [scope form]
+(defn interpret-in-scope [scope form]
   (let [ root-interpreter       (interpreter-for-scope-and-form scope form)
         ; TODO scope inheritance, and multiple inheritance
-         interpreter-arguments  (drop 1 form) ]
+         interpreter-arguments  (if (seq? form) (drop 1 form) [form]) ]
     (if root-interpreter
       ( let [ interpreter-result ( apply root-interpreter interpreter-arguments) ]
         ( if (interpreter-for-scope-and-form scope interpreter-result) (recur scope interpreter-result) interpreter-result ))
@@ -40,20 +43,26 @@
         )))
 
 
-(defmethod interpret-in-scope java.lang.String    [scope string] `(new StringLiteralExpr  ~string))
-(defmethod interpret-in-scope java.lang.Long      [scope long]   `(new LongLiteralExpr    ~(.toString long)))
-(defmethod interpret-in-scope java.lang.Boolean   [scope bool]   `(new BooleanLiteralExpr ~bool))
-(defmethod interpret-in-scope java.lang.Character [scope char]   `(new CharLiteralExpr    ~(.toString char)))
-(defmethod interpret-in-scope java.lang.Double    [scope double] `(new DoubleLiteralExpr  ~(.toString double)))
-(defmethod interpret-in-scope nil                 [scope & a]    `(new NullLiteralExpr ))
+;(defmethod interpret-in-scope java.lang.String    [scope string] `(new StringLiteralExpr  ~string))
+;(defmethod interpret-in-scope java.lang.Long      [scope long]   `(new LongLiteralExpr    ~(.toString long)))
+;(defmethod interpret-in-scope java.lang.Boolean   [scope bool]   `(new BooleanLiteralExpr ~bool))
+;(defmethod interpret-in-scope java.lang.Character [scope char]   `(new CharLiteralExpr    ~(.toString char)))
+;(defmethod interpret-in-scope java.lang.Double    [scope double] `(new DoubleLiteralExpr  ~(.toString double)))
+;(defmethod interpret-in-scope nil                 [scope & a]    `(new NullLiteralExpr ))
 
 (reset-scope :expression)
 (add-interpreters-to-scope
   :expression
   { '(quote break) (fn [] `(new BreakStmt) )
+    java.lang.String (fn [string] `(new StringLiteralExpr ~string))
    })
 
 
 (interpret-in-scope :expression
                     '('break)
                     )
+
+(interpret-in-scope :expression
+                    "Foo"
+                    )
+
