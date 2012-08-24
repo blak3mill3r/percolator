ca14855662cc27cca253f17d1475bd49d5a78dc2
Blake Miller <blak3mill3r@gmail.com>
2012-08-24 08:37:57 -0700

    ./play dependencies, clojure 1.4

--- play/project.clj
+++ play/project.clj
@@ -1,10 +1,9 @@
 (defproject play "1.0.0-SNAPSHOT"
   :description "for building GWT interfaces with clojure"
   :dependencies [
-                 [org.clojure/clojure                   "1.3.0" ]
+                 [org.clojure/clojure                   "1.4.0" ]
                  [com.google.code.javaparser/javaparser "1.0.8" ]
-                 [org.clojure/clojure-contrib           "1.2.0" ]
-                 [percolator/core                       "1.0.0" ]
+                 [percolator/percolator                 "1.0.0-SNAPSHOT" ]
                  ]
   :dev-dependencies [
                      [org.clojars.ibdknox/lein-nailgun "1.1.1" ]
