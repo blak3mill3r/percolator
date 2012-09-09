baacfcaf06dc955bbf690e2f8010a3c94695f1ba
Blake Miller <blak3mill3r@gmail.com>
2012-09-09 14:08:40 -0700

    rewrite interpreter
    this is much classier and more robust
    scope inheritance before this commit only went 1 level deep, it was not recursive
    now it is recursive, and also cyclic inheritance is no problem, it should work
    work as expected
    
    The rule is that within any strongly connected component of the inheritance graph,
    there can be no name collisions. Name collisions across different DCSs are fine,
    one masks the other in a defined way.
    
    Haven't tested it much yet but the example works and now some more complex
    interpreter architecture is possible

--- src/percolator/interpreter.clj
+++ src/percolator/interpreter.clj
@@ -1,53 +1,98 @@
 (in-ns 'percolator.core)
 
 (def interpreters {})
-(def scope-inheritance {})
+
+(defn reset-scope [scope]
+  (def interpreters (assoc interpreters scope {})))
 
 (defn add-interpreters-to-scope [scope new-interpreters]
   (let [ existing (interpreters scope)
          merged   (merge existing new-interpreters) ]
     ( def interpreters (assoc interpreters scope merged))))
 
-(defn inherit-scope [scope parent wrapper]
-  (let [ existing  (scope-inheritance scope)
-         modified  (conj existing [parent wrapper])
-        ]
-    (def scope-inheritance
-      (assoc scope-inheritance scope modified))))
+(def ^:dynamic *perc-scope-args* {})
 
-(defn reset-scope [scope]
-  (def interpreters
-    (assoc interpreters scope {}))
-  (def scope-inheritance
-    (assoc scope-inheritance scope [])))
+(defn tarjan [e v0]
+  (let [tarjan-helper 
+         (fn self [v s indices lowlinks index sccs]
+           (let [main ; the body of the forall loop
+                  (fn [[s indices lowlinks index sccs :as state] vp]
+                    (cond
+                      (nil? (indices vp)) ; Was successor v' visited? 
+                        (let [[s indices lowlinks index sccs] 
+                                (apply self vp state)
+                              lowlinks (assoc lowlinks v (min (lowlinks v) 
+                                                           (lowlinks vp)))]
+                          [s indices lowlinks index sccs])
+                      (some #{vp} s) ; Is v' on the stack? (crude)
+                        (let [lowlinks (assoc lowlinks v (min (lowlinks v) 
+                                                           (lowlinks vp)))]
+                          [s indices lowlinks index sccs])
+                      :else state))
 
-; NOTE the ordering matters if interpreter keys shadow each other
-; the LAST one you add with inherit-scope will take precedence in the event of a collision
-; undecided if that's Good Thing or not, maybe it should be other way around
-; in which case this is the place to start
-(defn inherited-interpreters [scope]
-  (let [ inherited-scopes (scope-inheritance scope)
-         interpreters-for-scopes 
-         (map #( interpreters ( first %1 ) ) inherited-scopes)
-        ]
-    (reduce merge {} interpreters-for-scopes) ))
+                 ; the forall loop is now a reduction on successors
+                 [s indices lowlinks index sccs] 
+                   (reduce main [(cons v s) 
+                                 (assoc indices v index) 
+                                 (assoc lowlinks v index) 
+                                 (inc index)
+                                 sccs] (e v))]
 
-(defn inherited-interpreter-wrapper [scope parent-scope]
-  (or
-    ((reduce merge {} (scope-inheritance scope))
-     parent-scope)
-    identity ))
+             (if (= (lowlinks v) (indices v))
+               ; conjoin the newly found scc to sccs and 
+               ; remove its vertices from the stack
+               (let [[a b] (split-with #(not= v %) s)
+                     sccs (conj sccs (concat a [v]))]
+                 [(rest b) indices lowlinks index sccs])
+               [s indices lowlinks index sccs])))]
 
-; a map of scope names to
-; maps of syntax keys to the (originating) scope name
-(defn scope-inheritance-name-map []
-  (reduce merge {} 
-    (reverse
-      (map (fn [a] (reduce
-                     (fn [map-to-scope-name [k v]] (assoc map-to-scope-name k (first a)))
-                     {}
-                     (last a)))
-      interpreters))))
+    (last (tarjan-helper v0 nil {} {} 0 []))))
+
+(def scope-dependencies {})
+
+(defn add-interpreters-to-scope [scope new-interpreters]
+  (let [ existing (interpreters scope)
+         merged   (merge existing new-interpreters) ]
+    ( def interpreters (assoc interpreters scope merged))))
+
+(def scope-inheritance-wrappers
+  "a map of scope-name keyword keys to maps of scope-name keyword keys to wrapper functions, used internally to store wrappers which are essentially edge-data in the scope dependency graph"
+  {})
+
+(defn inherit-scope [one-scope another-scope & wrapper]
+  "make /one-scope/ inherit /another-scope/"
+  (def scope-dependencies
+    (assoc scope-dependencies one-scope
+      (merge
+        (or (one-scope scope-dependencies) #{})
+        another-scope)))
+  (when-not (empty? wrapper)
+    (def scope-inheritance-wrappers
+      (assoc scope-inheritance-wrappers one-scope
+             (merge
+               (or (one-scope scope-inheritance-wrappers) {})
+               {another-scope (first wrapper)} )))))
+
+(defn inherit-scope-recur [scope]
+  "make /scope/ inherit itself"
+  (inherit-scope scope scope))
+
+
+(defn- scope-dependency-traversal [scope]
+  "return a list defining the search path for interpreters for the given scope"
+  (reverse (tarjan scope-dependencies scope)))
+
+(defn interpreters-and-sources-in-scope [scope]
+  "return a map of all interpreter keys available for a given scope to values which are 2-element vectors of [interpreter-fn, scope-name], produce an error for name collisions, TODO cache this"
+  (for [strongly-connected-scopes (scope-dependency-traversal scope)]
+    (letfn [ (complain-on-collision [one another] (throw (Throwable. "Shitty name collision")))
+             (keys-to-scope-name [scope-name]
+               (reduce #(assoc % %2 scope-name) {}
+                 (keys (interpreters scope-name))))
+             (name-collision-safe-merge [one another] (merge-with complain-on-collision one another)) ]
+          (reduce name-collision-safe-merge
+            ( map #(merge-with vector ( interpreters % ) (keys-to-scope-name %) ) strongly-connected-scopes )
+            ))))
 
 (defn extract-interpreter-key-from-form [form]
   (if
@@ -56,33 +101,49 @@
       ( = 'quote (first form) ) 
         (last form))))
 
-(defn interpreter-for-scope-and-form [scope form]
-  (let [ root-interpreters (or (scope interpreters) {})
-         interpreter-key   (if (seq? form) (extract-interpreter-key-from-form (first form)) (class form))
-        ]
-    (or
-      (root-interpreters interpreter-key)
-      (let [ inherited
-               (( inherited-interpreters scope) interpreter-key)
-             scope-inherited-from
-               ( (scope-inheritance-name-map) interpreter-key )
-             wrapper
-               ( inherited-interpreter-wrapper scope scope-inherited-from )
-            ]
-        (if inherited
-          (fn [& args] (wrapper (apply inherited args))))))))
-
-(def ^:dynamic *perc-scope-args* {})
-
 (defn interpret-in-scope [scope form]
-  (let [ interpreter            (interpreter-for-scope-and-form scope form)
-         interpreter-arguments  (if (seq? form) (drop 1 form) [form])
-         looks-percolatorish    (and (seq? form) (seq? (first form) ) (= 'quote (first (first form) )) ) ]  ; if no interpreter is found this is used to know whether to error out or eval the form
-    (if interpreter
-      ( let [ interpreter-result ( apply interpreter interpreter-arguments) ]
-        ( if (interpreter-for-scope-and-form scope interpreter-result)
-          (recur scope interpreter-result) interpreter-result ))
-      (if looks-percolatorish (println "PROBLEM interpreting, in scope (" scope ") this form:\n" form)
-        ( let [ eval-result (eval form) ]
-        ( if (interpreter-for-scope-and-form scope eval-result)
-          (recur scope eval-result) eval-result ))))))
+  "interpret percolator form /form/ in scope /scope/ returning a Java object representing the AST node(s)"
+  (let [interpreters-and-sources
+          (interpreters-and-sources-in-scope scope)
+        interpreter-arguments   
+          (if (seq? form)
+            (drop 1 form)
+            [form])
+        looks-percolatorish     
+          (fn [form] (and (seq? form)
+            (seq? (first form) )
+            (= 'quote (first (first form)))))  ; if no interpreter is found this is used to know whether to error out or eval the form
+        interpreter-key         
+          (if
+            (seq? form)
+            (extract-interpreter-key-from-form (first form))
+            (class form))
+        interpreter-and-source
+          (first (take 1 (drop-while not ; find the value from the first key that matches one of the maps in the vector
+            (for [scs interpreters-and-sources] (scs interpreter-key)))) )
+        interpreter-fn
+          (first interpreter-and-source)
+        interpreter-source
+          (last interpreter-and-source)
+        interpreter-wrapper-fn
+          (when interpreter-source
+            (or
+              (when (not (= scope interpreter-source)) ; not from current scope, check for wrapper fn
+                (interpreter-source (scope scope-inheritance-wrappers)))
+              identity))
+        ]
+    (if interpreter-fn
+      (let [result (interpreter-wrapper-fn ( apply interpreter-fn interpreter-arguments) )
+            ;recursive-result (interpret-in-scope scope result)
+            ]
+        (if (looks-percolatorish result) ; recur until it isn't a valid percolator form, then eval
+          (interpret-in-scope scope result)
+            result ))
+      ; no interpreter found in any inherited scope
+      (if ( looks-percolatorish form)
+        ( throw (Throwable. (string/join ["Shitty death with sources " (string/join (map #(.toString %) (first (take 1 interpreters-and-sources ) ) ) )  " on form " (.toString form) "\nscope "  scope " no interpreter-fn for key " (.toString interpreter-key ) " which looks like it should be a percolator form"] )))
+        (interpreter-wrapper-fn form )
+        ;( throw (Throwable. (string/join [ "Doesnt look percolatorish but I dont have a fn for it: " (.toString form ) ])) )
+        ) )))
+;( throw (Throwable. (string/join ["Shitty death on form " (.toString form) "\n no interpreter-fn for key " (.toString interpreter-key ) " which is a " (.toString (class interpreter-key))] )))
+ 
