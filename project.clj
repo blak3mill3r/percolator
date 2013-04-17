e8d124681f4f374afa2f9beb48131cfd675c533f
Blake Miller <blak3mill3r@gmail.com>
2013-04-17 12:40:59 -0700

    bump clojure to 1.5.1 and update for lein 2

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject percolator "1.0.0-SNAPSHOT"
   :description "for working with Java AST structures in Clojure"
   :dependencies [
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [com.google.code.javaparser/javaparser "1.0.8"]
                  ]
-  :dev-dependencies [[org.clojars.ibdknox/lein-nailgun "1.1.1"]
-                     [vimclojure/server "2.3.5"]]
             )
