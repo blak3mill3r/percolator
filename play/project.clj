df4e3d30a47b1b0f9e3bf88a7b1bb060b9aaa535
Blake Miller <blak3mill3r@gmail.com>
2013-04-17 12:43:03 -0700

    bump clojure to 1.5.1 and update for lein 2 for 'play' sample

--- play/project.clj
+++ play/project.clj
@@ -1,18 +1,12 @@
 (defproject play "1.0.0-SNAPSHOT"
   :description "for building GWT interfaces with clojure"
   :dependencies [
-                 [org.clojure/clojure                   "1.4.0" ]
+                 [org.clojure/clojure                   "1.5.1" ]
                  [com.google.code.javaparser/javaparser "1.0.8" ]
                  [percolator/percolator                 "1.0.0-SNAPSHOT" ]
+                 [com.google.gwt/gwt-user               "2.5.1" ]
+                 [com.google.gwt/gwt-dev                "2.5.1" ]
                  ]
-  :dev-dependencies [
-                     [org.clojars.ibdknox/lein-nailgun "1.1.1" ]
-                     [vimclojure/server                "2.3.5" ]
-                     [lein-gwt                         "0.1.1" ]
-                     [lein-percolator                  "0.0.1-SNAPSHOT" ]
-                     [com.google.gwt/gwt-user          "2.4.0" ]
-                     [com.google.gwt/gwt-dev           "2.4.0" ]
-                     ]
   :gwt-modules ["com.whatsys.Play"]
   :gwt-options {:localWorkers 1
                 :war "war"
